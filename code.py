f57797cb6c08814ffaff8e8fbddf2210abe4e8bf
mcoldco78 <51186043+mcoldco78@users.noreply.github.com>
2019-07-18 17:02:36 -0400

    Update code.py

--- code.py
+++ code.py
@@ -21,6 +21,7 @@ def choosePath():
     return path 
       
 def checkPath(chosenPath) :
+    time.sleep(2)
     print("The moment is now")
     time.sleep(2)
     print("You decided with your choice")
@@ -32,13 +33,18 @@ def checkPath(chosenPath) :
     correctPath = (1)
       
     if chosenPath == str(correctPath) :
+        time.sleep(2)
         print("You succusfully ran out the door")
+        time.sleep(2)
         print("You are running and make it to the police department down the street")
     else:
+        time.sleep(2)
         print("You hesitate, you're grabbed by a set of hands coming from the bathroom to your back")
+        time.sleep(2)
         print("You fade into the darkness and your fate is left with no question")
 
 def scene2():
+    time.sleep(2)
     print("1.You make a bolt to the stairs or 2.You hide in the closet in your bedroom")
 
 def scene3():
@@ -48,15 +54,53 @@ def scene3():
     print("Upon investigating,the police find nobody in the house and no scenes of a struggle.")
     time.sleep(5)
     print("However, the fusebox was discovered to had been turned off")
+    time.sleep(5)
+    print("As the police captain, do you send patrol around the surrounding neighborhood for the rest of the week to deter this coninuing or assume normal assignments? (1.Change and act on hard or 2.Do not change)")
+    time.sleep(5)
+
+
+#2nd choice
+def choosePath2():
+    path = ""
+    while path != "1" and path != "2":
+        path = input(" (1 or 2) : ")
+        
+    return path 
+      
+def checkPath2(chosenPath2) :
+    time.sleep(2)
+    print("Over-reacting will frighten the community")
+    time.sleep(2)
+    print("Not doing enough seems like you're a week captain")
+    time.sleep(2)
+    print("However, you have to stick to your decision as a captain")
+    print()
+    
+     
+    correctPath2 = (1)
+      
+    if chosenPath2 == str(correctPath2) :
+        time.sleep(2)
+        print("You over-react and assign to much of your force to the are, three nights later, same crime happens on the other side of town")
+        time.sleep(2)
+        print("The criminal(s) know how you act...")
+    else:
+        time.sleep(2)
+        print("You act cauaslly, and not over-assign your team")
+        time.sleep(2)
+        print("The person strikes again, but this time down the street")
+        time.sleep(2)
+        print("Same MO, you got a serial killer on your hands")
 
 
   
 playAgain = "yes"
-while playAgain == "yes" or playAgain == "y":
+while playAgain == "yes":
     scene1()
     scene2()
     choice = choosePath()
     checkPath(choice)
     scene3()
-    playAgain = input ("Do you want to play again? (yes or y to continue playing) : ")
-
+    choice2 = choosePath2()
+    checkPath2(choice2)
+    playAgain = input ("Do you want to play again? (yes to continue playing) : ")
